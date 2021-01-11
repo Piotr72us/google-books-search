11e71a4f9b2b98a1ebc72aca65119d0dfc66eff8
Piotr Dudek <piotr72@gmail.com>
2021-01-10 20:21:27 -0500

    working link in book card

--- client/src/components/carditem/index.js
+++ client/src/components/carditem/index.js
@@ -6,13 +6,14 @@ function CardItem(props) {
     <Card>
       <Row>
         <Col sm={10}>
-
           <h3 className="title padded">{props.title}</h3>
           <h6 className="author padded"> written by: {props.authors}</h6>
         </Col>
         <Col sm={2}>  
-        <Button className="btnEl" as="input" type="button" value="View" onClick={() => window.location.href="https://books.google.com/books?id=f280CwAAQBAJ&dq=harrypotter&hl=en&source=gbs_api"} />
-        <Button className="btnEl" as="input" type="button" value="Save" />
+          <a target="_blank" rel="noopener noreferrer" href={props.link}>
+            <Button className="btnEl" as="input" type="button" value="View"/>
+          </a>
+          <Button className="btnEl" as="input" type="button" value="Save" />
         </Col>
       </Row>
       <Row>
